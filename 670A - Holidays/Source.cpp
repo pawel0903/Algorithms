187b13827080868743dc005840ecbcc139a5a28c
pawel0903 <pawel0903@gmail.com>
2016-05-29 18:11:09 +0200

    Added CodeForces 670A - Holidays problem solution

---
+++ 670A - Holidays/Source.cpp
@@ -0,0 +1,20 @@
+/// <summary>
+/// Problem solution for http://codeforces.com/problemset/problem/670/A
+/// </summary>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int noOfWeeks = n / 7;
+	int remainder = n % 7;
+
+	int min = (noOfWeeks * 2) + (remainder > 5 ? remainder - 5 : 0);
+	int max = (noOfWeeks * 2) + (remainder > 2 ? 2 : remainder);
+
+	cout << min << " " << max << endl;
+}
