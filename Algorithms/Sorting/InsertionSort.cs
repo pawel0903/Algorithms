6b07fae3d55beff02cdc1ad3ceb6dc32cb6eec1a
pawel0903 <pawel0903@gmail.com>
2016-05-29 22:42:58 +0200

    Added Insertion Sort

---
+++ Algorithms/Sorting/InsertionSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Algorithms.Common.Utils;
+
+namespace Algorithms.Sorting
+{
+    public static partial class Sorting
+    {
+        /// <summary>
+        /// Insertion sort
+        /// Time Complexity: 
+        /// Worst case - O(n^2)
+        /// Best case - O(n)
+        /// Average - O(n^2)
+        /// Auxilary space: O(1)
+        /// </summary>
+        /// <typeparam name="T">Type - must implement IComparable<T></typeparam>
+        /// <param name="array">Collection to be sorted</param>
+        public static void InsertionSort<T>(this T[] array) where T : IComparable<T>
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                T key = array[i];
+                int j = i - 1;
+
+                while (j >= 0 && array[j].CompareTo(key) > 0)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j+1] = key;
+            }
+        }
+    }
+}
---
+++ Tests/Sorting/InsertionSort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.Sorting;
+
+namespace Tests.Sorting
+{
+    [TestClass]
+    public class InsertionSort
+    {
+        [TestMethod]
+        public void InsertionSortBasicTest()
+        {
+            int[] ints = { 10, 45, 15, 39, 21, 26 };
+            int[] copy = ints.Clone() as int[];
+            ints.InsertionSort();
+            Array.Sort(copy);
+
+            for (int i = 0; i < ints.Length; i++)
+            {
+                Assert.AreEqual(ints[i], copy[i]);
+            }
+        }
+    }
+}
