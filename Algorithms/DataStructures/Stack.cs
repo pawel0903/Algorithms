c9610a16a7e2f85acc513e84684e1a0fba61acee
pawel0903 <pawel0903@gmail.com>
2016-06-04 19:22:27 +0200

    Added Stack

---
+++ Algorithms/DataStructures/Stack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.DataStructures
+{
+    /// <summary>
+    /// Generic Stack implementation
+    /// </summary>
+    /// <typeparam name="T">Type</typeparam>
+    public class Stack<T> where T : IComparable<T>
+    {
+        private List<T> _stack;
+
+        public Stack()
+        {
+            _stack = new List<T>();
+        }
+
+        public bool IsEmpty()
+        {
+            return _stack.Count > 0 ? false : true;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new Exception("Stack is empty!");
+            }
+
+            return _stack[_stack.Count - 1];
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new Exception("Stack is empty!");
+            }
+
+            T result = _stack[_stack.Count - 1];
+            _stack.RemoveAt(_stack.Count - 1);
+            return result;
+        }
+
+        public void Push(T item)
+        {
+            _stack.Add(item);
+        }
+    }
+}
---
+++ Tests/DataStructures/Stack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Algorithms.DataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.DataStructures
+{
+    [TestClass]
+    public class Stack
+    {
+        [TestMethod]
+        public void StackBasicTest()
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Assert.AreEqual(stack.Peek(), 3);
+            Assert.AreEqual(stack.Pop(), 3);
+            Assert.AreEqual(stack.Pop(), 2);
+            Assert.AreEqual(stack.Pop(), 1);
+            Assert.AreEqual(stack.IsEmpty(), true);
+            try
+            {
+                stack.Peek();
+                Assert.Fail();
+            }
+            catch (Exception)
+            {
+            }
+            stack.Push(11);
+            Assert.AreEqual(stack.IsEmpty(), false);
+            Assert.AreEqual(stack.Pop(), 11);
+        }
+    }
+}
