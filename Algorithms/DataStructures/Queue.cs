4f0ef4c0cee6eef14c1d9624dffdc80fe051cb99
pawel0903 <pawel0903@gmail.com>
2016-06-09 13:22:25 +0200

    Added Queue implementation

---
+++ Algorithms/DataStructures/Queue.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.DataStructures
+{
+    /// <summary>
+    /// Generic Queue implementation using linked list
+    /// </summary>
+    /// <typeparam name="T">Type</typeparam>
+    public class Queue<T>
+    {
+        private readonly List<T> _queue;
+
+        public Queue()
+        {
+            _queue = new List<T>();
+        }
+
+        /// <summary>
+        /// Checks if the queue is empty
+        /// Time complexity O(1)
+        /// </summary>
+        /// <returns>true if queue is empty, false otherwise</returns>
+        public bool IsEmpty()
+        {
+            return _queue.Count == 0;
+        }
+
+        /// <summary>
+        /// Adds an item to the queue.
+        /// Time complexity O(1)
+        /// </summary>
+        /// <param name="item">item to be added</param>
+        public void Enqueue(T item)
+        {
+            _queue.Add(item);
+        }
+
+        /// <summary>
+        /// Removes an item from the queue. The items are popped in the same order in which they are pushed.
+        /// Time complexity O(1)
+        /// </summary>
+        /// <returns>removed item</returns>
+        public T Dequeue()
+        {
+            if (_queue.Count == 0)
+                throw new Exception("Queue is empty. Cannot Dequeue.");
+            T item = _queue.First();
+            _queue.Remove(item);
+            return item;
+        }
+
+        /// <summary>
+        /// Return the first element in queue.
+        /// Does not remove the element.
+        /// Throws exception if queue is empty
+        /// Time complexity O(1)
+        /// </summary>
+        /// <returns>First element in queue</returns>
+        public T Front()
+        {
+            if(IsEmpty())
+                throw new Exception("Queue is empty. Cannot peek the front element.");
+            return _queue.First();
+        }
+
+        /// <summary>
+        /// Return last element in queue.
+        /// Does not remove the element.
+        /// Throws exception if queue is empty
+        /// Time complexity O(1)
+        /// </summary>
+        /// <returns>Lase element in queue</returns>
+        public T Rear()
+        {
+            if (IsEmpty())
+                throw new Exception("Queue is empty. Cannot peek the last element.");
+            return _queue.Last();
+        }
+    }
+}
---
+++ Tests/DataStructures/Queue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.DataStructures;
+
+namespace Tests.DataStructures
+{
+    [TestClass]
+    public class Queue
+    {
+        [TestMethod]
+        public void QueueBasicTest()
+        {
+            Queue<int> queue = new Queue<int>();
+            Assert.AreEqual(queue.IsEmpty(), true);
+            for (int i = 1; i <= 5; i++)
+            {
+                queue.Enqueue(i);
+            }
+            Assert.AreEqual(queue.Front(), 1);
+            Assert.AreEqual(queue.Rear(), 5);
+            for (int i = 1; i <= 5; i++)
+            {
+                Assert.AreEqual(queue.Dequeue(), i);
+            }
+            Assert.AreEqual(queue.IsEmpty(), true);
+            try
+            {
+                queue.Dequeue();
+                Assert.Fail();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
