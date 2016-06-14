561797caf599f33d8193193b9d1b2349d1946e9a
pawel0903 <pawel0903@gmail.com>
2016-06-14 12:10:47 +0200

    Added DirectedGraph

---
+++ Algorithms/DataStructures/DirectedGraph.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.DataStructures
+{
+    /// <summary>
+    /// Undirect Graph implementation using adjacent list
+    /// </summary>
+    /// <typeparam name="T">Type</typeparam>
+    public class DirectedGraph<T> : IGraph<T>
+    {
+        private readonly Dictionary<T, List<T>> _graph;
+
+        /// <summary>
+        /// Counts number of edges in direct graph
+        /// </summary>
+        public int CountEdges => (from e in _graph.Values select e.Count).Sum();
+        /// <summary>
+        /// Counts number of vertices in undirect graph
+        /// </summary>
+        public int CountVertices => _graph.Keys.Count;
+
+        public DirectedGraph()
+        {
+            _graph = new Dictionary<T, List<T>>();
+        }
+
+        /// <summary>
+        /// Adds an edge to a graph
+        /// </summary>
+        /// <param name="src">first vertex</param>
+        /// <param name="dest">second vertex</param>
+        public void AddEdge(T src, T dest)
+        {
+            Add(src, dest);
+            CheckExistAndAdd(dest);
+        }
+
+        /// <summary>
+        /// Adds vertex to a graph
+        /// If exists does nothing
+        /// </summary>
+        /// <param name="vertex">vertex to be added to graph</param>
+        public void AddVertex(T vertex)
+        {
+            CheckExistAndAdd(vertex);
+        }
+
+        /// <summary>
+        /// Deletes vertex from graph
+        /// If doesnt exist does nothing
+        /// </summary>
+        /// <param name="src">first vertex</param>
+        /// <param name="dest">second vertex</param>
+        public void DeleteEdge(T src, T dest)
+        {
+            if (_graph.ContainsKey(src) && _graph.ContainsKey(dest))
+            {
+                _graph[src].Remove(dest);
+            }
+        }
+
+        /// <summary>
+        /// Deletes vertex from graph
+        /// If vertex doesnt exist does nothing
+        /// </summary>
+        /// <param name="vertex">vertex to be deleted</param>
+        public void DeleteVertex(T vertex)
+        {
+            if (_graph.ContainsKey(vertex))
+            {
+                foreach (var neighbour in _graph[vertex].ToList())
+                {
+                    _graph[neighbour].Remove(vertex);
+                }
+
+                _graph.Remove(vertex);
+            }
+        }
+
+        /// <summary>
+        /// Adds an edge to a graph
+        /// If vertices don't exists cretes them
+        /// </summary>
+        /// <param name="src">source</param>
+        /// <param name="dest">destination</param>
+        private void Add(T src, T dest)
+        {
+            CheckExistAndAdd(src);
+
+            foreach (var neighbour in _graph[src])
+            {
+                if (neighbour.Equals(dest))
+                    return;
+            }
+
+            _graph[src].Add(dest);
+        }
+
+        /// <summary>
+        /// Checks if vertex exist in graph
+        /// If yes does nothing, otherwise adds a key to a graph
+        /// </summary>
+        /// <param name="key"></param>
+        private void CheckExistAndAdd(T key)
+        {
+            if (!_graph.ContainsKey(key))
+                _graph.Add(key, new List<T>());
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (var vertex in _graph.Keys)
+            {
+                result.Append(vertex);
+                foreach (var neighbour in _graph[vertex])
+                {
+                    result.Append(" -> ").Append(neighbour);
+                }
+                result.Append(Environment.NewLine);
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ Tests/DataStructures/DirectedGraph.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.DataStructures;
+
+namespace Tests.DataStructures
+{
+    [TestClass]
+    public class DirectedGraph
+    {
+        [TestMethod]
+        public void DirectedGraphBasicTest()
+        {
+            IGraph<int> graph = new DirectedGraph<int>();
+            Assert.AreEqual("", graph.ToString());
+
+            graph.AddEdge(0, 1);
+            string t1 = String.Format("{0} -> {1}{2}{1}{2}", 0, 1, Environment.NewLine);
+            Assert.AreEqual(t1, graph.ToString());
+            
+            graph.AddEdge(2, 0);
+            string t2 = String.Format("{0} -> {1}{3}{1}{3}{2} -> {0}{3}", 0, 1, 2, Environment.NewLine);
+            Assert.AreEqual(t2, graph.ToString());
+            string t3 = String.Format("{0} -> {1} -> {2}{3}{1}{3}{2} -> {0}{3}", 0, 1, 2, Environment.NewLine);
+            graph.AddEdge(0, 2);
+            Assert.AreEqual(t3, graph.ToString());
+            
+            graph.AddVertex(3);
+            string t4 = String.Format("{0}{1}{2}", t3, 3, Environment.NewLine);
+            Assert.AreEqual(t4, graph.ToString());
+            
+            Assert.AreEqual(4, graph.CountVertices);
+            Assert.AreEqual(3, graph.CountEdges);
+        }
+
+        [TestMethod]
+        public void DirectedGraphDeleteVertexTest()
+        {
+            IGraph<int> graph = new DirectedGraph<int>();
+            graph.AddEdge(0, 1);
+            graph.AddEdge(2, 0);
+            graph.AddVertex(3);
+
+            graph.DeleteVertex(3);
+            graph.DeleteVertex(2);
+
+            string t1 = String.Format("{0} -> {1}{2}{1}{2}", 0, 1, Environment.NewLine);
+            Assert.AreEqual(t1, graph.ToString());
+
+            graph.DeleteVertex(0);
+            graph.DeleteVertex(0);
+
+            string t2 = String.Format("{0}{1}", 1, Environment.NewLine);
+            Assert.AreEqual(graph.ToString(), t2);
+
+            graph.DeleteVertex(1);
+            Assert.AreEqual(graph.ToString(), "");
+            Assert.AreEqual(graph.CountVertices, 0);
+            Assert.AreEqual(graph.CountEdges, 0);
+        }
+
+        [TestMethod]
+        public void DirectedGraphDeleteEdgeTest()
+        {
+            IGraph<int> graph = new DirectedGraph<int>();
+            graph.AddEdge(0, 1);
+            graph.AddEdge(2, 0);
+            graph.AddEdge(3, 2);
+
+            graph.DeleteEdge(0, 1);
+            graph.DeleteEdge(0, 2);
+
+            Assert.AreEqual(graph.CountEdges, 2);
+
+            graph.DeleteEdge(2, 0);
+            graph.DeleteEdge(3, 2);
+
+            string t = "";
+            for (int i = 0; i < 4; i++)
+                t += String.Format("{0}{1}", i, Environment.NewLine);
+
+            Assert.AreEqual(t, graph.ToString());
+            Assert.AreEqual(0, graph.CountEdges);
+            Assert.AreEqual(4, graph.CountVertices);
+        }
+    }
+}
