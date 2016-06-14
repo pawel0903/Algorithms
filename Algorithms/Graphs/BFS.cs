ab456cceccd53cfbc51e40362fd5769d4cc43dd6
pawel0903 <pawel0903@gmail.com>
2016-06-14 17:08:26 +0200

    Added BFS algorithm

---
+++ Algorithms/Graphs/BFS.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.DataStructures;
+
+namespace Algorithms.Graphs
+{
+    public static partial class GraphAlgorithms
+    {
+        /// <summary>
+        /// Traverse graph in breadth first order
+        /// Returns traverse path as List
+        /// Time Complexity: O(V+E)
+        /// where V is number of vertices in the graph and E is number of edges in the graph
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="graph">Graph to be traversed</param>
+        /// <param name="first">start point in graph</param>
+        /// <returns>Traverse path</returns>
+        public static List<T> BreadthFirstSearch<T>(IGraph<T> graph, T first)
+        {
+            List<T> result = new List<T>();
+            DataStructures.Queue<T> queue = new DataStructures.Queue<T>();
+            HashSet<T> visited = new HashSet<T>();
+
+            queue.Enqueue(first);
+
+            while (!queue.IsEmpty())
+            {
+                T current = queue.Dequeue();
+                result.Add(current);
+                List<T> neighbours = graph.GetNeighbours(current);
+                visited.Add(current);
+
+                foreach (var neighbour in neighbours)
+                {
+                    if (!visited.Contains(neighbour))
+                    {
+                        queue.Enqueue(neighbour);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tests/Graph/BFS.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.DataStructures;
+using static Algorithms.Graphs.GraphAlgorithms;
+
+namespace Tests.Graph
+{
+    [TestClass]
+    public class BFS
+    {
+        [TestMethod]
+        public void BreadthFirstSearchBasicTest()
+        {
+            IGraph<int> graph = new UndirectGraph<int>();
+            graph.AddEdge(1, 1);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(1, 3);
+            graph.AddEdge(1, 4);
+            graph.AddEdge(2, 5);
+            graph.AddEdge(2, 6);
+            graph.AddEdge(4, 7);
+            graph.AddEdge(4, 8);
+            graph.AddEdge(5, 9);
+            graph.AddEdge(5, 10);
+            graph.AddEdge(7, 11);
+            graph.AddEdge(7, 12);
+
+            List<int> traverseOrder = BreadthFirstSearch(graph, 1);
+            for (int i = 0; i < traverseOrder.Count; i++)
+            {
+                Assert.AreEqual(i+1, traverseOrder[i]);
+            }
+        }
+    }
+}
