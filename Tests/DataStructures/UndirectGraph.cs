47346ba82947c365686626f372a4950e5ec0670a
pawel0903 <pawel0903@gmail.com>
2016-06-14 14:05:22 +0200

    UndirectGraph - test refactor

--- Tests/DataStructures/UndirectGraph.cs
+++ Tests/DataStructures/UndirectGraph.cs
@@ -11,24 +11,24 @@ namespace Tests.DataStructures
         public void UndirectGraphBasicTest()
         {
             UndirectGraph<int> graph = new UndirectGraph<int>();
-            Assert.AreEqual(graph.ToString(), "");
+            Assert.AreEqual("", graph.ToString());
 
             graph.AddEdge(0, 1);
             string t1 = String.Format("{0} -> {1}{2}{1} -> {0}{2}", 0, 1, Environment.NewLine);
-            Assert.AreEqual(graph.ToString(), t1);
+            Assert.AreEqual(t1, graph.ToString());
 
             graph.AddEdge(2, 0);
             string t2 = String.Format("{0} -> {1} -> {2}{3}{1} -> {0}{3}{2} -> {0}{3}", 0, 1, 2, Environment.NewLine);
-            Assert.AreEqual(graph.ToString(), t2);
+            Assert.AreEqual(t2, graph.ToString());
             graph.AddEdge(0, 2);
-            Assert.AreEqual(graph.ToString(), t2);
+            Assert.AreEqual(t2, graph.ToString());
 
             graph.AddVertex(3);
             string t3 = String.Format("{0}{1}{2}", t2, 3, Environment.NewLine);
-            Assert.AreEqual(graph.ToString(), t3);
+            Assert.AreEqual(t3, graph.ToString());
 
-            Assert.AreEqual(graph.CountVertices, 4);
-            Assert.AreEqual(graph.CountEdges, 2);
+            Assert.AreEqual(4, graph.CountVertices);
+            Assert.AreEqual(2, graph.CountEdges);
         }
 
         [TestMethod]
@@ -43,18 +43,18 @@ namespace Tests.DataStructures
             graph.DeleteVertex(2);
 
             string t1 = String.Format("{0} -> {1}{2}{1} -> {0}{2}", 0, 1, Environment.NewLine);
-            Assert.AreEqual(graph.ToString(), t1);
+            Assert.AreEqual(t1, graph.ToString());
 
             graph.DeleteVertex(0);
             graph.DeleteVertex(0);
 
             string t2 = String.Format("{0}{1}", 1, Environment.NewLine);
-            Assert.AreEqual(graph.ToString(), t2);
+            Assert.AreEqual(t2, graph.ToString());
 
             graph.DeleteVertex(1);
-            Assert.AreEqual(graph.ToString(), "");
-            Assert.AreEqual(graph.CountVertices, 0);
-            Assert.AreEqual(graph.CountEdges, 0);
+            Assert.AreEqual("", graph.ToString());
+            Assert.AreEqual(0, graph.CountVertices);
+            Assert.AreEqual(0, graph.CountEdges);
         }
 
         [TestMethod]
@@ -68,7 +68,7 @@ namespace Tests.DataStructures
             graph.DeleteEdge(0, 1);
             graph.DeleteEdge(0, 2);
 
-            Assert.AreEqual(graph.CountEdges, 1);
+            Assert.AreEqual(1, graph.CountEdges);
 
             graph.DeleteEdge(2, 3);
 
@@ -76,9 +76,9 @@ namespace Tests.DataStructures
             for (int i = 0; i < 4; i++)
                 t += String.Format("{0}{1}", i, Environment.NewLine);
 
-            Assert.AreEqual(graph.ToString(), t);
-            Assert.AreEqual(graph.CountEdges, 0);
-            Assert.AreEqual(graph.CountVertices, 4);
+            Assert.AreEqual(t, graph.ToString());
+            Assert.AreEqual(0, graph.CountEdges);
+            Assert.AreEqual(4, graph.CountVertices);
         }
     }
 }
