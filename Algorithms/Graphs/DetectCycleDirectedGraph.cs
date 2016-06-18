79ff39ea7c839a9a502c9f956e470b69c1d01346
pawel0903 <pawel0903@gmail.com>
2016-06-18 15:02:07 +0200

    Added detection for cycle in directed graph

---
+++ Algorithms/Graphs/DetectCycleDirectedGraph.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using Algorithms.DataStructures;
+
+namespace Algorithms.Graphs
+{
+    public static partial class GraphAlgorithms
+    {
+        /// <summary>
+        /// Check if joint graph contains cycle
+        /// </summary>
+        /// <typeparam name="T">type</typeparam>
+        /// <param name="graph">graph</param>
+        /// <param name="vertex">current vertex</param>
+        /// <param name="visited">visited vertices</param>
+        /// <param name="recStack">used for checking for back egdes</param>
+        /// <returns>true if graph contains cycle, false otherwise</returns>
+        private static bool IsCyclicUtil<T>(DirectedGraph<T> graph, T vertex, ref HashSet<T> visited, ref HashSet<T> recStack)
+        {
+            if (!visited.Contains(vertex))
+            {
+                visited.Add(vertex);
+                recStack.Add(vertex);
+
+                foreach (var neighbour in graph.GetNeighbours(vertex))
+                {
+                    if (!visited.Contains(neighbour) && IsCyclicUtil(graph, neighbour, ref visited, ref recStack))
+                        return true;
+
+                    if (recStack.Contains(neighbour))
+                        return true;
+                }
+            }
+
+            recStack.Remove(vertex);
+
+            return false;
+        }
+
+        /// <summary>
+        /// Checks if directed graph contains cycle
+        /// </summary>
+        /// <typeparam name="T">type</typeparam>
+        /// <param name="graph">graph</param>
+        /// <returns>true if graph contains cycle, false otherwise</returns>
+        public static bool IsCyclic<T>(DirectedGraph<T> graph)
+        {
+            HashSet<T> visited = new HashSet<T>();
+            HashSet<T> recStack = new HashSet<T>();
+
+            foreach (var vertex in graph.GetVertices())
+            {
+                if (IsCyclicUtil(graph, vertex, ref visited, ref recStack))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Tests/Graph/DetectCycleDirectedGraph.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Algorithms.DataStructures;
+using static Algorithms.Graphs.GraphAlgorithms;
+
+namespace Tests.Graph
+{
+    [TestClass]
+    public class DetectCycleDirectedGraph
+    {
+        [TestMethod]
+        public void DetectCycleDirectedGraphBasicTest()
+        {
+            var graph = new DirectedGraph<int>();
+            graph.AddEdge(1, 2);
+            graph.AddEdge(1, 3);
+            graph.AddEdge(2, 3);
+            graph.AddEdge(3, 4);
+
+            Assert.AreEqual(false, IsCyclic(graph));
+
+            graph.AddEdge(3, 3);
+
+            Assert.AreEqual(true, IsCyclic(graph));
+
+            graph.DeleteEdge(3, 3);
+            graph.AddEdge(4, 2);
+
+            Assert.AreEqual(true, IsCyclic(graph));
+
+            graph.DeleteEdge(4, 2);
+            graph.AddEdge(8, 8);
+
+            Assert.AreEqual(true, IsCyclic(graph));
+        }
+    }
+}
